1287484bf8e7e6abf4bfc42536b956d8f83224d3
ariceliom <contato@ariceliomoura.com>
2023-12-16 23:39:10 -0300

    mensagem

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Olá dev!")
+// Variáveis
+let nome = "Lázaro";
+let xp = 7600;
+
+// Função para obter o nível com base na experiência
+function obterNivel(xp) {
+  switch (true) {
+    case xp < 1000:
+      return "Ferro";
+    case xp <= 2000:
+      return "Bronze";
+    case xp <= 5000:
+      return "Prata";
+    case xp <= 6000:
+      return "Ouro";
+    case xp <= 7000:
+      return "Platina";
+    case xp <= 8000:
+      return "Diamante";
+    case xp <= 9000:
+      return "Ascendente";
+    default:
+      return "Erro";
+  }
+}
+
+// Obtém e exibe o nível
+let nivel = obterNivel(xp);
+console.log(`O herói ${nome} está no nível ${nivel}.`);
